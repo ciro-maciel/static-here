a370606655fce6e13fc6a05772314c7f0e1d7675
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-05-07 12:52:21 -0300

    - add class person para melhor exemplo de code

--- src/assets/js/application.js
+++ src/assets/js/application.js
@@ -1,16 +1,25 @@
+var moment = require('moment');
+let utility = require('utility');
+import Children from 'person';
+import {
+    work,
+    Person
+} from 'person';
 
- 
- var moment = require('moment');
- let utility = require('utility') ;
+
+var edgar = new Children('Edgar');
+console.log(edgar.doWork());
+
+console.log(work('Andrea'));
+
+
 
 console.log(window)
 console.log(utility.square(7))
 console.log(moment().format('LLLL'))
 
-window.onload = function(){
+window.onload = function () {
 
     console.log('Hi!!')
 
 }
-
- 
---
+++ src/assets/js/library/person.js
@@ -0,0 +1,22 @@
+
+
+function work(name) {
+    return `${name} is working`;
+}
+
+
+class Person {
+
+    constructor(name){
+        this.name = name;
+    }
+
+    doWork(){
+        return work(this.name);
+    }
+
+}
+
+
+export {work, Person}
+export default Person
